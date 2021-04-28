6457bb873873af3e7daf8a54c8da2d35902cd420
tleisara <chaowuttisuk@hotmail.com>
2021-04-28 17:00:43 +0700

    Update msl2elipsoid.py

--- msl2elipsoid.py
+++ msl2elipsoid.py
@@ -1,8 +1,7 @@
 import subprocess
 import pandas as pd
-import pdb
 from pyproj import Proj, transform
-import os
+
 
 GEIOEVAL_TGM2017 =  'geoideval -n tgm2017-1 --input-string "{} {}"'
 inProj = Proj(init='epsg:32647')
@@ -11,28 +10,28 @@ outProj = Proj(init='epsg:4326')
 def towgs84(df, col_E, col_N):
     return transform(inProj, outProj, df[col_E], df[col_N])
 
-def GeoidUndul_TGM2017( lat, lng): # ฟังก์ชันในการหาค่า N โดยเรียกใช้ command ด้วย subprocess
-    cmd = GEIOEVAL_TGM2017.format( lat, lng )# แทนค่า parameter(lat,lng) ลงในคำสั่ง commmand
+def GeoidUndul_TGM2017( lat, lng):
+    cmd = GEIOEVAL_TGM2017.format( lat, lng )
     result = subprocess.run( cmd,  shell=True, check=True , capture_output=True )
-    # run command แล้วเรียกใช้คำสั่งที่กำหนดไว้ในตัวแปร CMD และเก็บผลลัพธ์ไว้ใน Result
-    undul = float( result.stdout)# ตัดตัวอักษรที่ไม่เกี่ยวข้องออกด้วย .stdout และแปลงเป็น float
-    return undul # คืนค่า Geoid Undulation ; N (meter)
+    undul = float( result.stdout)
+    return undul 
 
-# path CSV
-path = '/Users/isara/Documents/GeographicLib/GCP_CU_Saraburi.csv'
-
+# read CSV
+path = '/Users/isara/Documents/GeographicLib/***.csv'
 df = pd.read_csv(path)
 
 # transform to wgs84
-wgs84_arr = towgs84(df, df.columns[1], df.columns[2])
-df['lat'] = wgs84_arr[1]
-df['lon'] = wgs84_arr[0]
+col_E_name = df.columns[1]
+col_N_name = df.columns[2]
+wgs84_arr = towgs84(df, col_E_name, col_N_name)
+df['lat'], df['lon'] = wgs84_arr[1], wgs84_arr[0]
 
 # get Undulation
 df['undulation'] = df.apply( lambda x: GeoidUndul_TGM2017(x.lat, x.lon) , axis=1 )
 
-# get elipsoid
+# get elipsoid height
 df['h'] = df[df.columns[3]] + df['undulation']
 df['h'] = df['h'].round(4)
-# Export
+
+# Export to csv
 df.to_csv(path.replace('.csv', '_elipsoid.csv'))
